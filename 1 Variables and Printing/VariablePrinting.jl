25b34373547a638f41b6b3fd7cd2b53e2dea4dc0
Jaemin Eun <jeun@terpmail.umd.edu>
2020-12-05 15:37:53 -0500

    Variables and Printing

---
+++ 1 Variables and Printing/VariablePrinting.jl
@@ -0,0 +1,11 @@
+# Jaemin Eun
+# Julia Lesson #1
+
+### Variables and Printing ###
+
+#How To Print
+World = "Julia"
+println("Hello"," ",World)
+#To Concatenate, use a comma instead of a + sign. Also, can add spaces by spacing quotes
+
+#How To Assign Variables
