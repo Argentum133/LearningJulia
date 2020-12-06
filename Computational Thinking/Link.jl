9d95ed1344c8986ab8c00c313d125bdf3c454e70
Jaemin Eun <jeun@terpmail.umd.edu>
2020-12-05 21:08:39 -0500

    Pluto

--- Computational Thinking/Link.jl
+++ Computational Thinking/Link.jl
@@ -5,6 +5,10 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 0c7295c0-3766-11eb-1e22-0183bb9b1673
+# Jaemin Eun
+# Computational Thinking
+
+#https://computationalthinking.mit.edu/Fall20/
 
 
 # ╔═╡ Cell order:
